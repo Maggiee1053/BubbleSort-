311fb75be844ae2791ca5d77307d75f6501f1360
Maggiee1053 <86923417+Maggiee1053@users.noreply.github.com>
2022-01-24 17:34:40 +0530

    Add files via upload

---
+++ Bubblesort.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class BubbleSort{
+public static void printarray(int arr[]){
+System.out.println("sorted list is");
+for( int i=0; i<arr.length; i++ ){
+System.out.println(arr[i]+" ");
+}
+System.out.println();
+}
+public static  void main(String args[]){
+Scanner sc= new Scanner(System.in);
+int arr[] = new int[6];
+for(int i=0; i<arr.length; i++){
+arr[i]=sc.nextInt();
+}
+for(int i=0 ; i<(arr.length-1) ; i++){
+for(int j=0; j<(arr.length-i-1); j++){
+if(arr[j] > arr[j+1] ){
+int temp = arr[j];
+arr[j]= arr[j+1];
+arr[j+1] = temp ;
+}
+}
+}
+printarray(arr);
+}
+}
+
